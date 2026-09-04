8248094faf1e51e35d4d4ee91cd83fb5f6de4b30
Zynx <theleader1201@gmail.com>
2026-09-04 22:18:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 const toggle = document.querySelector(".toggle");
 
-if (localStorage.getItem("theme") === "light") {
-    document.body.classList.add("light");
-}
-
-toggle?.addEventListener("click", () => {
+toggle.addEventListener("click", () => {
     document.body.classList.toggle("light");
-    localStorage.setItem(
-        "theme",
-        document.body.classList.contains("light") ? "light" : "dark"
-    );
-});
 
-window.addEventListener("load", () => {
-    document.body.classList.add("loaded");
+    toggle.textContent =
+        document.body.classList.contains("light")
+            ? "Dark"
+            : "Theme";
 });
